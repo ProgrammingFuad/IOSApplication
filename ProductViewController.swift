e48618d010577b8f47deb08fb833bd017b8ff281
Fuad <Fuad.Mohamoud@und.edu>
2016-07-18 20:45:26 -0500

    First iOS Application
    My first IOS application.

---
+++ ProductViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Fuad Mohamoud on 3/6/16.
+//  Copyright © 2016 Fuad Mohamoud. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+}
